c113dbb525ea7fed1a0d571a650acaf85287903f
fuivu <fuivu@users.noreply.github.com>
2026-08-10 11:10:12 +0800

    Fix: use correct coil library alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -140,5 +140,5 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.datastore.preferences)
-    implementation(libs.coil)
+    implementation(libs.coil.compose)
 }
